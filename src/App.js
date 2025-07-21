56476a73fca95285c6b78a75c30dc0c3becbdf3d
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-07-21 09:20:02 +0530

    'test'

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
