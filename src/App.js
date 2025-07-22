85a77caf6f586c833ceaf01d478a74120b3b2b2e
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-07-22 23:08:08 +0530

    'test'

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ProductDetail from "./pages/ProductDetail";
 import { useState } from "react";
 import {ToastContainer} from 'react-toastify'; 
 import 'react-toastify/dist/ReactToastify.css';
+import Cart from "./pages/Cart";
 
 function App() {
    const [cartItems, SetCartItems] = useState([])
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<Home />} />
             <Route path='/product/:id' element ={<ProductDetail cartItems={cartItems} SetCartItems={SetCartItems} />} />
+            <Route path='/cart' element={<Cart cartItems={cartItems} SetCartItems={SetCartItems}/>} />
           </Routes>
         </div>
       </Router>
